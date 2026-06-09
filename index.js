a51494db0e99e1a8840e1e2e5265be75204bf63f
Shivam Yadav <shivamydv.work25@gmail.com>
2026-06-09 19:44:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const modal = document.querySelector(".modal");
-//const overlay = document.querySelector(".overlay");
+ const overlay = document.querySelector(".overlay");
 
-function openmodal (){
-    //console.log("Modal is Open ");
-    modal.classList.add("active");
-    //overlay.classList.add("overlayactive");
+ // modal open function 
 
-};
+ const openModal=()=>{
+    console.log("modal is Open");
+    modal.classList.add('active');
+    overlay.classList.add('overlayactive');
+ };
 
-function closemodal (){
-    modal.classList.remove("active");
-    //overlay.classList.remove("overlayactive");
+  // modal close function 
 
-};
+ const closeModal=()=>{
+    modal.classList.remove('active');
+    overlay.classList.remove('overlayactive');
+ };
 
